07dc25013c80702f8651e0a0c69f690c5d1eecfa
Gabriel Kurzak <gabrielkurzak@gmail.com>
2022-07-04 18:37:25 +0200

    Initial commit - Project initialization

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1> Initalization of BGGF project </h1>
---
+++ boardgames_project/boardgames_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BoardgamesAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'boardgames_app'
